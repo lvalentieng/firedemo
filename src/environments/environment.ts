7fee011d010883749da8fe9be7b223edd7e65bcb
lvalenti <leonardo.valenti@eng.it>
2025-10-13 12:00:12 +0200

    add tests components

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDG9Cy7URLw0QeuOEga-ISj0OlA1Hh_tb4",
+        authDomain: "firedemo-leo.firebaseapp.com",
+        projectId: "firedemo-leo",
+        storageBucket: "firedemo-leo.firebasestorage.app",
+        messagingSenderId: "761874954741",
+        appId: "1:761874954741:web:407c215bd9aff21d3748bc",
+        measurementId: "G-YJKQEGTR9X"
+    }
+};
