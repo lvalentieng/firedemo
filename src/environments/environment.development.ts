8322c33f6d34884f16ebfb172a264860a82313ec
lvalenti <leonardo.valenti@eng.it>
2025-10-13 12:13:11 +0200

    add angularfire

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,10 +3,12 @@ export const environment = {
     firebaseConfig: {
         apiKey: "AIzaSyDG9Cy7URLw0QeuOEga-ISj0OlA1Hh_tb4",
         authDomain: "firedemo-leo.firebaseapp.com",
-        projectId: "firedemo-leo",
+        projectId: "firedemo-leo", 
+        projectNumber: "761874954741",
         storageBucket: "firedemo-leo.firebasestorage.app",
         messagingSenderId: "761874954741",
         appId: "1:761874954741:web:407c215bd9aff21d3748bc",
-        measurementId: "G-YJKQEGTR9X"
+        measurementId: "G-YJKQEGTR9X", 
+        version: "2"
     }
 };
